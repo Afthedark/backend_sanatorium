0cc469254d8c34e7b21a20eb2014215aecdb4bc2
Luis Gonzalo <afthedark@hotmail.com>
2024-12-14 20:45:47 -0400

    add first commit branch dev

--- proyect_sanatorium/settings.py
+++ proyect_sanatorium/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'proyect_sanatorium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbsanatorium',
+        'USER': 'adminluis',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
